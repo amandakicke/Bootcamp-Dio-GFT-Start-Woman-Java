873acbaae15e69e508f44afcce09f14e790bea21
amandakicke <amandakicke@gmail.com>
2022-04-26 13:52:52 -0300

    commit

---
+++ Dominando IDEs Java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato = new Gato("Bichano", "Branco", 2);
+		System.out.println(gato);
+		
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println(a + b);
+		System.out.println("Hello, World!!!");*/
+	}
+
+}

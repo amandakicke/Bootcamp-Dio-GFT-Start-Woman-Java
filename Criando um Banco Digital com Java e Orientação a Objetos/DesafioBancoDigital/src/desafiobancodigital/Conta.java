3e6868f3087361b0be3c92d4cc2b9fd05adc7b8c
amandakicke <amandakicke@gmail.com>
2022-05-25 09:28:38 -0300

    commit

---
+++ Criando um Banco Digital com Java e Orientação a Objetos/DesafioBancoDigital/src/desafiobancodigital/Conta.java
@@ -0,0 +1,78 @@
+
+package desafiobancodigital;
+
+public abstract class Conta implements IConta{
+    
+    private static final int AGENCIA_PADRAO = 001;
+    private static int SEQUENCIAL = 1;
+    
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+ 
+       
+    public Conta(){
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+    
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+    
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    protected void imprimirInfosComuns(){
+        System.out.println(String.format("Agência: %d", this.agencia));
+        System.out.println(String.format("Número: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+          
+    public void sacar(){
+        
+    }
+    
+    public void depositar(){
+                
+    }
+    public void transferir(){
+        
+    }
+
+
+}
+    
---
+++ Criando um Banco Digital com Java e Orientação a Objetos/DesafioBancoDigital/src/desafiobancodigital/DesafioBancoDigital.java
@@ -0,0 +1,18 @@
+
+package desafiobancodigital;
+
+public class DesafioBancoDigital {
+
+    public static void main(String[] args) {
+    
+        Conta cc = new ContaCorrente();
+        cc.depositar(100);
+        
+        Conta cp = new ContaPoupanca();
+        
+        cc.transferir(10, cp);
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+    }
+    
+}
